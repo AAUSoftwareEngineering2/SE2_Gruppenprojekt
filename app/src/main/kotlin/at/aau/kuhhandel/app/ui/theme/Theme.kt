69503033097b7cfc6b12f8219a8c6007e77cf83c
neckritz <ole.neckritz@gmx.de>
2026-04-12 15:21:32 +0200

    Fixed wrong indentation causing styling error

--- app/src/main/kotlin/at/aau/kuhhandel/app/ui/theme/Theme.kt
+++ app/src/main/kotlin/at/aau/kuhhandel/app/ui/theme/Theme.kt
@@ -6,25 +6,27 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = DefaultPurple,
-    secondary = DarkPurple,
-    tertiary = LightPurple,
-    background = PureWhite,
-    surface = WhitePurple,
-    onPrimary = Color.White,
-    onBackground = Color.White
-)
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = DefaultPurple,
+        secondary = DarkPurple,
+        tertiary = LightPurple,
+        background = PureWhite,
+        surface = WhitePurple,
+        onPrimary = Color.White,
+        onBackground = Color.White
+    )
 
-private val LightColorScheme = lightColorScheme(
-    primary = DefaultPurple,
-    secondary = DarkPurple,
-    tertiary = LightPurple,
-    background = PureWhite,
-    surface = WhitePurple,
-    onPrimary = Color.White,
-    onBackground = DarkPurple
-)
+private val LightColorScheme =
+    lightColorScheme(
+        primary = DefaultPurple,
+        secondary = DarkPurple,
+        tertiary = LightPurple,
+        background = PureWhite,
+        surface = WhitePurple,
+        onPrimary = Color.White,
+        onBackground = DarkPurple
+    )
 
 @Composable
 fun AndroidAppTheme(
