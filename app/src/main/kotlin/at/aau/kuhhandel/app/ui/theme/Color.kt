9ba6d88a5ad8e30f1c99ff34a8bcfc7e5530faec
neckritz <ole.neckritz@gmx.de>
2026-04-12 15:12:50 +0200

    feat: Introduced Color Scheme and Font

---
+++ app/src/main/kotlin/at/aau/kuhhandel/app/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package at.aau.kuhhandel.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Default UI colors
+val BackgroundGreen = Color(0xFFC3F37D)
+val DefaultPurple = Color(0xFFA175B2)
+val DarkPurple = Color(0xFF473D47)
+val LightPurple = Color(0xFFDFCFE5)
+val WhitePurple = Color(0xFFFAF7FF)
+val PureWhite = Color(0xFFFFFFFF)
--- app/src/main/kotlin/at/aau/kuhhandel/app/ui/theme/Theme.kt
+++ app/src/main/kotlin/at/aau/kuhhandel/app/ui/theme/Theme.kt
@@ -4,9 +4,27 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme()
-private val LightColorScheme = lightColorScheme()
+private val DarkColorScheme = darkColorScheme(
+    primary = DefaultPurple,
+    secondary = DarkPurple,
+    tertiary = LightPurple,
+    background = PureWhite,
+    surface = WhitePurple,
+    onPrimary = Color.White,
+    onBackground = Color.White
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = DefaultPurple,
+    secondary = DarkPurple,
+    tertiary = LightPurple,
+    background = PureWhite,
+    surface = WhitePurple,
+    onPrimary = Color.White,
+    onBackground = DarkPurple
+)
 
 @Composable
 fun AndroidAppTheme(
@@ -16,6 +34,7 @@ fun AndroidAppTheme(
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     MaterialTheme(
         colorScheme = colorScheme,
+        typography = Typography,
         content = content,
     )
 }
