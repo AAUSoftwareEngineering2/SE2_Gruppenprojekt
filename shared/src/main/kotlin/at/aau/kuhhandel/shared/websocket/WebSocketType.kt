148a168f8173d1e82eb04de328a8004cf6f68b12
hesysthenos <268232512+hesysthenos@users.noreply.github.com>
2026-06-05 19:46:33 +0200

    feat(shared): add new WebSocket types
    Added CHANGE_SETTINGS, RESET_TO_LOBBY, SUBMIT_TRADE_MONEY, SPY, and CATCH_SPY in accordance with the state machine diagram.

--- shared/src/main/kotlin/at/aau/kuhhandel/shared/websocket/WebSocketType.kt
+++ shared/src/main/kotlin/at/aau/kuhhandel/shared/websocket/WebSocketType.kt
@@ -6,16 +6,21 @@ import kotlinx.serialization.Serializable
 enum class WebSocketType {
     // Client commands
     CREATE_GAME,
+    CHANGE_SETTINGS,
     START_GAME,
+    RESET_TO_LOBBY,
     JOIN_GAME,
     LEAVE_GAME,
     CHOOSE_AUCTION,
     RECONNECT,
     CHOOSE_TRADE,
+    SUBMIT_TRADE_MONEY,
     RESPOND_TO_TRADE,
     PLACE_BID,
     RESOLVE_AUCTION,
     FINISH_TRADE_REVEAL,
+    SPY,
+    CATCH_SPY,
 
     // Server events
     GAME_CREATED,
