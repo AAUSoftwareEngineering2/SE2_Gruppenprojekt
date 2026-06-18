974bf547e4c3ce18d524c0e2e6ade2366bea1a38
hesysthenos <268232512+hesysthenos@users.noreply.github.com>
2026-06-18 03:43:28 +0200

    refactor(shared): remove unnecessary files left from original project

--- shared/src/main/kotlin/at/aau/kuhhandel/shared/Constants.kt
+++
@@ -1,7 +0,0 @@
-package at.aau.kuhhandel.shared
-
-/** App-wide constants shared across all modules. */
-object Constants {
-    const val APP_VERSION = "1.0.0"
-    const val DEFAULT_PORT = 8080
-}
